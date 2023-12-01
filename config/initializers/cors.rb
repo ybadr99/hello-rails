5b21c99013848562429e931f4a52f20d68474a65
ybadr99 <yousefbadr909@gmail.com>
2023-12-02 01:12:12 +0200

    update cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,10 +2,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://127.0.0.1:3000', 'http://localhost:3000'
+        origins 'http://localhost:5173'
         resource '*',
           headers: :any,
-          methods: [:get, :post, :patch, :put, :delete, :options, :head]
+          methods: [:get, :post, :put, :delete, :options, :head],
+          credentials: false
       end
     end
     
